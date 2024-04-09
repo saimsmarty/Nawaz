791ca354934d68006cf5ea3ed0b8d6e31a450210
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-04-09 11:25:13 +0530

    Update allahhafiz.js

--- Nawaz/commands/allahhafiz.js
+++ Nawaz/commands/allahhafiz.js
@@ -19,7 +19,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Allah Hafiz")) {
 		var msg = {
 				body: "★𝐁𝐘𝐞 , 𝐀𝐥𝐥𝐚𝐡 𝐇𝐚𝐟𝐢𝐳 𝐁𝐚𝐛𝐮 😍𓆪᭄🩷🪽✦`🥀🦋",
-				attachment: fs.createReadStream(__dirname + `/noprefix/Nawaz.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/Bye.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😍", event.messageID, (err) => {}, true)
