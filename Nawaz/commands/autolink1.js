293d358b47f0c86a3bcbcc0792712ce7923e22f6
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-04-16 11:32:29 +0530

    Update autolink1.js

--- Nawaz/commands/autolink1.js
+++ Nawaz/commands/autolink1.js
@@ -1,42 +1,73 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+const tinyurl = require('tinyurl');
+
 module.exports.config = {
-	name: "autolink",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "SINGU-💌💌",
-	description: "tự động gửi link về cho admin",
-	commandCategory: "Tiện ích",
-	usages: "",
-	cooldowns: 5
+  name: "autodl",
+  version: "1.0.",
+  hasPermssion: 0,
+  credits: "Dipto",
+  description: "Fb Vid Downloader",
+  commandCategory: "other",
+  usages: "fb video link",
+   usePrefix: true,
+  cooldowns: 2
 };
-module.exports.run = async function({ api , event , args }) {
-  const axios = require("axios");
-  const fs = require("fs-extra");
-    console.log('Hello, world !');
-};
-module.exports.handleEvent = async function({ api , event , Users }) {
-    const { body , senderID , threadID } = event;
-  const moment = require("moment-timezone");
-  const time = moment.tz("Asia/Ho_Chi_Minh").format("DD/MM/YYYY || HH:mm:ss");
-  const fs = require("fs-extra");
-    try {
-        if (body === undefined || !body.includes('repl.co/') || senderID == api.getCurrentUserID() || senderID == '') return;
-      if (body === undefined || !body.includes('replit.com/@') || senderID == api.getCurrentUserID() || senderID == '') return;
-      if (body === undefined || !body.includes('run.mocky.io') || senderID == api.getCurrentUserID() || senderID == '') return;
-      if (body === undefined || !body.includes('https://pastebin.com/raw/') || senderID == api.getCurrentUserID() || senderID == '') return;
-        var name = await Users.getNameUser(event.senderID);
-        var nameBox = global.data.threadInfo.get(event.threadID).threadName || "❌𝐓𝐞̂𝐧 𝐛𝐨𝐱 𝐤𝐡𝐨̂𝐧𝐠 𝐭𝐨̂̀𝐧 𝐭𝐚̣𝐢";
-        api.sendMessage(`📥=== [ 𝗔𝗨𝗧𝗢 𝗟𝗜𝗡𝗞 𝗪𝗘𝗕 ] ===📥
-━━━━━━━━━━━━━━━
-⏰ 𝗩𝗮̀𝗼 𝗹𝘂́𝗰: ${time}
-👥 𝗡𝗴𝘂̛𝗼̛̀𝗶 𝗱𝘂̀𝗻𝗴: ${name}
-🌍 𝗡𝗵𝗼́𝗺: ${nameBox}
-🌸 𝗩𝘂̛̀𝗮 𝗴𝘂̛̉𝗶 𝗺𝗼̣̂𝘁 𝗻𝗼̣̂𝗶 𝗱𝘂𝗻𝗴 𝗰𝗼́ 𝗰𝗵𝘂̛́𝗮 𝗹𝗶𝗻𝗸
-💬 𝗡𝗼̣̂𝗶 𝗱𝘂𝗻𝗴 𝗰𝗵𝘂̛́𝗮 𝗹𝗶𝗻𝗸: ${body}`,
-'100013942628281');
-// api.sendMessage(`📥=== [ 𝗔𝗨𝗧𝗢 𝗟𝗜𝗡𝗞 𝗪𝗘𝗕 ] ===📥
-// ━━━━━━━━━━━━━━━━
-// ⏰ 𝗩𝗮̀𝗼 𝗹𝘂́𝗰: ${dtai}\n📇 𝗡𝗴𝘂̛𝗼̛̀𝗶 𝗱𝘂̀𝗻𝗴: ${name}\n⚠️ 𝗩𝘂̛̀𝗮 𝗰𝗼́ 𝗴𝘂̛̉𝗶 𝟭 𝗹𝗶𝗻𝗸\n🤖 𝗕𝗼𝘁 𝘀𝗲̃ 𝗴𝘂̛̉𝗶 𝘃𝗲̂̀ 𝗰𝗵𝗼 𝗮𝗱𝗺𝗶𝗻 𝗵𝘂́𝗽`, event.threadID, event.messageID);
-    } catch (e) {
-        api.sendMessage(`${e}`, '100013942628281');
+
+module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
+let dipto = event.body ? event.body : '';
+  try {
+if (dipto.startsWith('https://vt.tiktok.com') ||
+dipto.startsWith("https://vm.tiktok.com") ||
+dipto.startsWith('https://www.facebook.com') || 
+dipto.startsWith('https://fb.watch')||
+dipto.startsWith('https://www.instagram.com/')|| dipto.startsWith('https://youtu.be/') ||
+dipto.startsWith('https://www.instagram.com/p/') || dipto.startsWith('https://pin.it/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://www.capcut.com/') || dipto.startsWith('https://www.threads.net/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://l.likee.video/')){
+  api.sendMessage("wait baby", event.threadID, event.messageID);
+  if (!dipto) {
+    api.sendMessage("please put a valid fb video link", event.threadID, event.messageID);
+    return;
     }
+const aa = await axios.get(`https://noobs-api.onrender.com/dipto/alldl?url=${encodeURIComponent(dipto)}`);
+   const bb = aa.data;
+   const shortUrl = await tinyurl.shorten(bb.result);
+   const MSG = `✅ 🔗 Download Url: ${shortUrl}`;
+   let ex;
+   let cp;
+        if (bb.result.includes('.jpg')){
+             ex = ".jpg";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.png')){
+             ex = ".png";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.jpeg')){
+             ex = ".jpeg";
+             cp = "Here's your Photo <😘";
+        }
+        else { 
+          ex = ".mp4";
+          cp = bb.cp;
+        }
+const path = __dirname + `/cache/video${ex}`;
+    const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
+    fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
+    api.sendMessage({
+      body: `${cp}\n${MSG}\n 🥹💙`,
+      attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
+if (dipto.startsWith('https://i.imgur.com')){
+  const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
+  const response = await axios.get(dipto, { responseType: 'arraybuffer' });
+const filename = __dirname + `/cache/dipto${dipto3}`;
+    fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
+    api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
+  () => fs.unlinkSync(filename),event.messageID)
+}
+} catch (e) {
+api.sendMessage(`${e}`, event.threadID, event.messageID);
+  };
 };
+exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
