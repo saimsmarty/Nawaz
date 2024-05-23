37c74d836dfa40a340fd55b38c459353480f48f4
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-05-23 15:26:45 +0530

    Update 0autodown.js

--- Nawaz/commands/0autodown.js
+++ Nawaz/commands/0autodown.js
@@ -1,55 +1,73 @@
 const axios = require('axios');
 const fs = require('fs-extra');
+const tinyurl = require('tinyurl');
 
-module.exports = {
-  config: {
-    name: "alldl", 
-    version: "1.0.1",
-    credits: "Dipto",
-    cooldowns: 2,
-    hasPermission: 0,
-    description: {
-      en: "Download video from tiktok, facebook, Instagram, YouTube, and more"
-    },
-    commandCategory: "media",
-},
+module.exports.config = {
+  name: "autodl",
+  version: "1.0.",
+  hasPermssion: 0,
+  credits: "Dipto",
+  description: "Fb Vid Downloader",
+  commandCategory: "other",
+  usages: "fb video link",
+   usePrefix: true,
+  cooldowns: 2
+};
 
-  run: async function ({ api, args, event, message }) {
-    const dipto = event.messageReply?.attachments[0]?.url || args[0];
-
-    if (!dipto){
-       api.setMessageReaction("❌", event.messageID, (err) => {}, true);
-    }
+module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
+let dipto = event.body ? event.body : '';
   try {
-       api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
-
-    const path = __dirname + `/cache/dip.mp4`;
-
-    const { data } = await axios.get(`${global.GoatBot.config.API.DIPTO}/alldl?url=${encodeURIComponent(dipto)}`);
-
-    const vid = (await axios.get(data.result, { responseType: "arraybuffer", })).data;
-
+if (dipto.startsWith('https://vt.tiktok.com') ||
+dipto.startsWith("https://vm.tiktok.com") ||
+dipto.startsWith('https://www.facebook.com') || 
+dipto.startsWith('https://fb.watch')||
+dipto.startsWith('https://www.instagram.com/')|| dipto.startsWith('https://youtu.be/') ||
+dipto.startsWith('https://www.instagram.com/p/') || dipto.startsWith('https://pin.it/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://www.capcut.com/') || dipto.startsWith('https://www.threads.net/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://l.likee.video/')){
+  api.sendMessage("", event.threadID, event.messageID);
+  if (!dipto) {
+    api.sendMessage("please put a valid fb video link", event.threadID, event.messageID);
+    return;
+    }
+const aa = await axios.get(`${global.config.API}/dipto/alldl?url=${encodeURIComponent(dipto)}`);
+   const bb = aa.data;
+   const shortUrl = await tinyurl.shorten(bb.result);
+   const MSG = `💝 🔗 Download Url: ${shortUrl}💘`;
+   let ex;
+   let cp;
+        if (bb.result.includes('.jpg')){
+             ex = ".jpg";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.png')){
+             ex = ".png";
+             cp = "Here's your Photo <😘";
+        }
+        else if (bb.result.includes('.jpeg')){
+             ex = ".jpeg";
+             cp = "Here's your Photo <😘";
+        }
+        else { 
+          ex = ".mp4";
+          cp = bb.cp;
+        }
+const path = __dirname + `/cache/video${ex}`;
+    const vid = (await axios.get(bb.result, { responseType: "arraybuffer", })).data;
     fs.writeFileSync(path, Buffer.from(vid, 'utf-8'));
-     api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-
-    api.sendMessage({ 
-      body: `🙂🤍`, 
-      attachment: fs.createReadStream(path),
-    }, event.threadID, () => fs.unlinkSync(path), event.messageID);
+    api.sendMessage({
+      body: `${cp}\n${MSG}\n ♻️𝗖𝗥𝗘𝗗𝗜𝗧:𝗥𝗞𝗢 𝗕𝗥𝗢💝💞`,
+      attachment: fs.createReadStream(path) }, event.threadID, () => fs.unlinkSync(path), event.messageID)}
 if (dipto.startsWith('https://i.imgur.com')){
-const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
-
+  const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
   const response = await axios.get(dipto, { responseType: 'arraybuffer' });
-
 const filename = __dirname + `/cache/dipto${dipto3}`;
-
     fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
-    api.sendMessage({body: `✅ | Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
+    api.sendMessage({body: `Downloaded from link`,attachment: fs.createReadStream(filename)},event.threadID,
   () => fs.unlinkSync(filename),event.messageID)
 }
-} catch (error) {
-    api.setMessageReaction("❎", event.messageID, (err) => {}, true);
-api.sendMessage(error, event.threadID, event.messageID);
-  }
-  },
-      }
+} catch (e) {
+api.sendMessage(`${e}`, event.threadID, event.messageID);
+  };
+};
+module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
