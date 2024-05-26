9e82609f6b02bc0c06fbef83b4d90a12a9ac1ac0
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-05-26 17:39:21 +0530

    Update 00dallee.js

--- Nawaz/commands/00dallee.js
+++ Nawaz/commands/00dallee.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 module.exports.config = {
     name: "dalle2",
     version: "1.0",
-    credits: "dipto",
+    credits: "rko",
     hasPermssion: 0,
     usePrefix: true,
     description: "Generate images by Dalle-3 AI",
@@ -18,12 +18,12 @@ module.exports.run = async function ({ api, event, args }) {
    return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
   }
     try {
-      //const cookies = "1YxCeslN567z6zeZvxBAz1aOMyRmqphJ6DS_VhQzeCJ32NZjsf_9U4F9T4glONdNW7zF-oGToPFAu_UNt02c7tcwA1G4Vqe_QbZU18v7fz-0r4V_jrT0ZA8Jbt-4FhK14tnnx7hZGxYO7s4qkcdnV7tvkK1YXkqAgAUUp7L7XrYxITrzqZMwyajvVIej8DwuJiMYJpEB9Erz-NabiE2P-qnSyGq2I4ryDpE0lCRFDseQ";
-const tl = [" 1YxCeslN567z6zeZvxBAz1aOMyRmqphJ6DS_VhQzeCJ32NZjsf_9U4F9T4glONdNW7zF-oGToPFAu_UNt02c7tcwA1G4Vqe_QbZU18v7fz-0r4V_jrT0ZA8Jbt-4FhK14tnnx7hZGxYO7s4qkcdnV7tvkK1YXkqAgAUUp7L7XrYxITrzqZMwyajvVIej8DwuJiMYJpEB9Erz-NabiE2P-qnSyGq2I4ryDpE0lCRFDseQ","cookies 2"];
+      //const cookies = "cookies here (_U value)";
+const tl = ["1MPG0WJUEa40k7YGsfQrA7EcBB1RkzKqO6XSTTMgpkPTH3zuXXV6jqciplKAcDxUr8xKvRtMh2xPm_PsT03sc8jWqb-lZV0hlP_4AxxWzZT5vXWL1R0Z4dHerGOrNT7I1v03N89pHYTA1xjUirTopaLcX25HjuEg9eovlW52HTx93tPOdcGRPRoGJqsz-3RnDCUGQ9wFYUg6GMc_hljg3lg","1wbiMIQjO9BOerw6Nuz0dnJxKt3LIE1Hr2Uatk_okCb9IotK3dwmJMCLjJWXmL1ZwJbBhUCwDg_G106efKF9SOf3YfWkY5UuPWvThbfVjtv6JdDER8kHz_2N_CgZKGnacrtOQqFB6gkiSEX5hblEqYTbUHXdAJvDZzzp4r2VqGoJpbE1_NyEZWTCjxhN4ggigbnqEVthDE1BFoueF7_YlKw"];
 const cookies = tl[Math.floor(Math.random() * tl.length)];
       const w = await api.sendMessage("Wait gara baby < 😽", event.threadID);
   
-const response = await axios.get(`https://noobs-api2.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`)
+const response = await axios.get(`https://noobs-api2.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=${col}`)
       const data = response.data.imgUrls;
       if (!data || data.length === 0) {
         api.sendMessage("No images generated.",event.threadID,event.messageID);
