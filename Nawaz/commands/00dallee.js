8c23d517842b4d6567a592ede2bacdc845c80a4e
nawazahmad0 <159174106+nawazahmad0@users.noreply.github.com>
2024-05-30 12:50:17 +0530

    Update 00dallee.js

--- Nawaz/commands/00dallee.js
+++ Nawaz/commands/00dallee.js
@@ -1,48 +1,44 @@
-const axios = require('axios');
-const fs = require('fs-extra');
 module.exports.config = {
-    name: "dalle2",
-    version: "1.0",
-    credits: "rko",
-    hasPermssion: 0,
-    usePrefix: true,
-    description: "Generate images by Dalle-3 AI",
-    commandCategory: "download",
-    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k]",
-    cooldowns: 5
-  };
+  name: "dalle2",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "RKO BRO",
+  description: "",
+  commandCategory: "Image",
+  usages: "dalle cat",
+  cooldowns: 5
+};
 
-module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
-  }
-    try {
-      //const cookies = "cookies here (_U value)";
-const tl = ["1YxCeslN567z6zeZvxBAz1aOMyRmqphJ6DS_VhQzeCJ32NZjsf_9U4F9T4glONdNW7zF-oGToPFAu_UNt02c7tcwA1G4Vqe_QbZU18v7fz-0r4V_jrT0ZA8Jbt-4FhK14tnnx7hZGxYO7s4qkcdnV7tvkK1YXkqAgAUUp7L7XrYxITrzqZMwyajvVIej8DwuJiMYJpEB9Erz-NabiE2P-qnSyGq2I4ryDpE0lCRFDseQ"];
-const cookies = tl[Math.floor(Math.random() * tl.length)];
-      const w = await api.sendMessage("Wait gara baby < 😽", event.threadID);
-  
-const response = await axios.get(`https://noobs-api2.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=${col}`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("No images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = __dirname + `/cache/${i + 1}.jpg`;
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `✅ |Naw Baby Tumar Generated Pic<😘`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const prompt = args.join(" ");
+    const key = this.config.credits;
+    if (!prompt) return api.sendMessage('use : /bing cat', event.threadID, event.messageID); 
+
+    const rndm = ['1rYfuivaMv34Kq0h-Ds6PZd_45kNfDBmsAc_En0GinOEiIkbw1lzhvl53z-e5BTLA043F-aDGTPJWYuZiI_tZd59ksWfTTFZpiWOK0QFzDf5TL-cuGhYNApGnMbymjPEiV3whAKnzARzZNPdgRLrN8YfxSJOhmfY36U3FtLhwOtt-G2li_5CeTkNJoS5fvOX_sUtI30ICDmea2CpqyoeR1WSTBU-X2wauftLx7AqJ4eE'];  //input cooki here
+    var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+
+    const res = await axios.get(`https://bing-api-5dpl.onrender.com/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+    console.log(res.data);
+    const data = res.data.result;
+    const numberSearch = data.length;
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+        let path = __dirname + `/cache/${num += 1}.jpg`;
+        let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+        fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+        imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
-  };
+
+    await api.sendMessage({
+        attachment: imgData,
+        body: "🥀Dalle Search Result💝\n\nPrompt: " + prompt + "\n\n#🌹Number of Images💓: " + numberSearch
+    }, event.threadID, event.messageID); 
+
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`);
+    }
+};
