add253ab29b0cfe9188db11913dcf03333f12d3d
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-02-28 09:28:54 +0530

    Update 0bing.js

--- Nawaz/commands/0bing.js
+++ Nawaz/commands/0bing.js
@@ -1,44 +1,48 @@
 module.exports.config = {
-  name: "dalle",
-  version: "1.0.0",
+  name: "itom",
+  version: "2.0.8",
   hasPermssion: 0,
-  credits: "Nayan",
-  description: "",
-  commandCategory: "Image",
-  usages: "dalle cat",
-  cooldowns: 5
+  credits: ".",
+  description: "ITOM",
+  commandCategory: "utilities",
+  usages: "cmdname [question]",
+  cooldowns: 5,
+  dependencies: {
+        "openai": ""
+    }
 };
-
 module.exports.run = async function({ api, event, args }) {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const prompt = args.join(" ");
-    const key = this.config.credits;
-    if (!prompt) return api.sendMessage('use : /bing cat', event.threadID, event.messageID); 
+ 
 
-    const rndm = ['1HBgb6DgyOcm9ZQ_ma_dwyPq63JkOnb1HH0M3T8Vp4xVuRsuWXQnku33aVX4rG42ukSSF89DEKfRXWXDNcNXE0LyohiwuYRO7ZnPA_JUyuppiR-rwysE6oCE0st7H9waTz7_XN4ycIDK1ZJYKuWFhhgPWH_XrYkhZmPrMOT5Zs0Nqe45NFMnZIBbPzk4diRs0Z-dkB2snG_YG8qfy0j8bwZMcabtCmrT0IrFOhGDRj7k '];  //input cooki here
-    var cookie = rndm[Math.floor(Math.random() * rndm.length)];
-
-    const res = await axios.get(`https://bing-api-5dpl.onrender.com/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
-
-    console.log(res.data);
-    const data = res.data.result;
-    const numberSearch = data.length;
-    var num = 0;
-    var imgData = [];
-    for (var i = 0; i < parseInt(numberSearch); i++) {
-        let path = __dirname + `/cache/${num += 1}.jpg`;
-        let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
-        fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-        imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
-    }
-
-    await api.sendMessage({
-        attachment: imgData,
-        body: "🥀Dalle Search Result💝\n\nPrompt: " + prompt + "\n\n#🌹Number of Images💓: " + numberSearch
-    }, event.threadID, event.messageID); 
-
-    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`);
-    }
-};
+const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+                  apiKey: "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",
+                            });
+                            const openai = new OpenAIApi(configuration);
+  let data = args.join(" ");
+                            if (data.length < 2) {
+                                api.sendMessage("?ai<⚠️ask anything👑> credits by  nawaz ahmad", event.threadID);
+                            } else {
+                                try {
+                                    const completion = await openai.createCompletion({
+                                        model: "text-davinci-003",
+                                        prompt: args.join(" "),
+                                        temperature: 0.5,
+                                        max_tokens: 2048,
+                                        top_p: 0.3,
+                                        frequency_penalty: 0.5,
+                                        presence_penalty: 0.0,
+                                    });
+                                    api.sendMessage(completion.data.choices[0].text, event.threadID, event.messageID);
+                                }
+                                catch (error) {
+                                    if (error.response) {
+                                        console.log(error.response.status);
+                                        console.log(error.response.data);
+                                    } else {
+                                        console.log(error.message);
+                                        api.sendMessage(error.message, event.threadID);
+                                    }
+                                }
+                            }
+}
