6e3ec3565a41f125b264b033ffdeed0764a4e9be
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-03-30 17:58:13 +0530

    Update unsend.js

--- Nawaz/commands/unsend.js
+++ Nawaz/commands/unsend.js
@@ -3,22 +3,21 @@ module.exports.config = {
     version: "1.0.1",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "Auto Unsend bot messages",
+    description: "Bot ke message unsend kare",
     commandCategory: "system",
     usages: "unsend",
     cooldowns: 0
 };
 
-module.exports.handleEvent = function({ api, event }) {
-    // अगर कोई बॉट के मैसेज पर 🫰 react करता है, तो वह मैसेज Unsend हो जाएगा।
-    if (event.type === "message_reaction" && event.reaction === "🫰") {
+module.exports.handleEvent = function ({ api, event }) {
+    if (event.reaction && event.reaction == "🫰") {
         api.unsendMessage(event.messageID);
     }
 };
 
-module.exports.run = function({ api, event, getText }) {
-    if (event.type !== "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-    if (event.messageReply.senderID !== api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+module.exports.run = function ({ api, event, getText }) {
+    if (!event.messageReply) return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+    if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
     
     return api.unsendMessage(event.messageReply.messageID);
 };
