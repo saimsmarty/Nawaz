e2237ffbca153e993651996ba7f0e186fe615d09
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-04-07 13:54:01 +0530

    Update setavt.js

--- Nawaz/commands/setavt.js
+++ Nawaz/commands/setavt.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "setavt",
-  version: "1.0.0",
-  hasPermssion: 2, // Only admin use
+  version: "1.0.1",
+  hasPermssion: 2,
   credits: "Nawaz Boss",
   description: "Reply image with +setavt to change bot profile picture",
   commandCategory: "admin",
@@ -9,23 +9,23 @@ module.exports.config = {
   cooldowns: 5,
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.run = async ({ api, event }) => {
   const fs = require("fs-extra");
   const axios = require("axios");
 
-  if (!event.messageReply || !event.messageReply.attachments[0]) {
-    return api.sendMessage("भाई फोटो पर reply कर के use कर ना।", event.threadID, event.messageID);
+  if (!event.messageReply || event.messageReply.attachments.length == 0) {
+    return api.sendMessage("Reply किसी Photo पे कर के +setavt लिख भाई!", event.threadID, event.messageID);
   }
 
-  const url = event.messageReply.attachments[0].url;
-  const path = __dirname + "/setavt.jpg";
+  const imgURL = event.messageReply.attachments[0].url;
+  const imgPath = __dirname + "/cache/avt.jpg";
 
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-  fs.writeFileSync(path, Buffer.from(response.data, "utf-8"));
+  const response = await axios.get(imgURL, { responseType: 'arraybuffer' });
+  fs.writeFileSync(imgPath, Buffer.from(response.data, "utf-8"));
 
-  api.changeAvatar(fs.createReadStream(path), (err) => {
-    if (err) return api.sendMessage("DP चेंज करने में error आया भाई!", event.threadID, event.messageID);
-    api.sendMessage("बॉट की DP Successfully Change हो गई भाई!", event.threadID, event.messageID);
-    fs.unlinkSync(path);
+  api.changeAvatar(fs.createReadStream(imgPath), (err) => {
+    if (err) return api.sendMessage("DP Change Failed!", event.threadID, event.messageID);
+    fs.unlinkSync(imgPath);
+    api.sendMessage("» Bot DP Change हो गया Boss!", event.threadID, event.messageID);
   });
 };
