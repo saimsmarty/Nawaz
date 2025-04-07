e68d05857c2847b0f03be7d3463db4f56a8d7bc6
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-04-07 14:00:21 +0530

    Update setavt.js

--- Nawaz/commands/setavt.js
+++ Nawaz/commands/setavt.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   name: "setavt",
-  version: "1.0.1",
+  version: "1.0.2",
   hasPermssion: 2,
   credits: "Nawaz Boss",
   description: "Reply image with +setavt to change bot profile picture",
@@ -9,23 +9,22 @@ module.exports.config = {
   cooldowns: 5,
 };
 
-module.exports.run = async ({ api, event }) => {
+module.exports.run = async function ({ api, event }) {
   const fs = require("fs-extra");
   const axios = require("axios");
 
   if (!event.messageReply || event.messageReply.attachments.length == 0) {
-    return api.sendMessage("Reply किसी Photo पे कर के +setavt लिख भाई!", event.threadID, event.messageID);
+    return api.sendMessage("Reply किसी image पर करो भाई।", event.threadID, event.messageID);
   }
 
-  const imgURL = event.messageReply.attachments[0].url;
-  const imgPath = __dirname + "/cache/avt.jpg";
+  const imgUrl = event.messageReply.attachments[0].url;
+  const path = __dirname + "/cache/avt.jpg";
 
-  const response = await axios.get(imgURL, { responseType: 'arraybuffer' });
-  fs.writeFileSync(imgPath, Buffer.from(response.data, "utf-8"));
+  const response = await axios.get(imgUrl, { responseType: "arraybuffer" });
+  fs.writeFileSync(path, Buffer.from(response.data, "utf-8"));
 
-  api.changeAvatar(fs.createReadStream(imgPath), (err) => {
-    if (err) return api.sendMessage("DP Change Failed!", event.threadID, event.messageID);
-    fs.unlinkSync(imgPath);
-    api.sendMessage("» Bot DP Change हो गया Boss!", event.threadID, event.messageID);
+  api.changeAvatar(fs.createReadStream(path), event.threadID, () => {
+    fs.unlinkSync(path);
+    api.sendMessage("✓ Bot की Profile Pic Change हो गई!", event.threadID, event.messageID);
   });
 };
