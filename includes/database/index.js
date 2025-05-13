1351ebace0d9d71d4489bb0277883895654bb643
Waring <159174106+nawazahmad0@users.noreply.github.com>
2025-05-13 12:19:48 +0530

    Update index.js

--- includes/database/index.js
+++ includes/database/index.js
@@ -7,35 +7,35 @@ dialect = dialect[0];
 storage = resolve(__dirname, `../${DATABASE[dialect].storage}`);
 
 module.exports.sequelize = new Sequelize({
-	dialect,
-	storage,
-	pool: {
-		max: 20,
-		min: 0,
-		acquire: 60000,
-		idle: 20000
-	},
-	retry: {
-		match: [
-			/SQLITE_BUSY/,
-		],
-		name: 'query',
-		max: 20
-	},
-	logging: false,
-	transactionType: 'IMMEDIATE',
-	define: {
-		underscored: false,
-		freezeTableName: true,
-		charset: 'utf8',
-		dialectOptions: {
-			collate: 'utf8_general_ci'
-		},
-		timestamps: true
-	},
-	sync: {
-		force: false
-	}
+  dialect,
+  storage,
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 60000,
+    idle: 20000
+  },
+  retry: {
+    match: [
+      /SQLITE_BUSY/,
+    ],
+    name: 'query',
+    max: 20
+  },
+  logging: false,
+  transactionType: 'IMMEDIATE',
+  define: {
+    underscored: false,
+    freezeTableName: true,
+    charset: 'utf8',
+    dialectOptions: {
+      collate: 'utf8_general_ci'
+    },
+    timestamps: true
+  },
+  sync: {
+    force: false
+  }
 });
 
 module.exports.Sequelize = Sequelize;
